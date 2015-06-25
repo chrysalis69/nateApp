bdebccb2e1baa9e6c51384c31646a6d7330608ed
Stefan Coetzee <stefan.coetzee@uct.ac.za>
2015-06-25 09:59:43 +0200

    Proxy settings

--- recipes/proxy_server.rb
+++ recipes/proxy_server.rb
@@ -1,8 +1,13 @@
+web_servers = node['nateApp']['webservers']
+
 include_recipe "haproxy::install_package"
 
 haproxy_lb 'nateAppWeb' do
   bind '0.0.0.0:80'
   mode 'http'
+  servers web_servers.each do |web|
+    #{web.name} #{web.ipaddress}:8080 check 
+  end
   params({
     'maxconn' => 20000,
     'balance' => 'leastconn'
