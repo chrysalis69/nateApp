b1e35b62d38a81517879341b155590333df05e9b
Stefan Coetzee <stefan.coetzee@uct.ac.za>
2015-06-25 11:05:19 +0200

    Fixed typo

--- recipes/proxy_server.rb
+++ recipes/proxy_server.rb
@@ -2,8 +2,8 @@ include_recipe "haproxy::install_package"
 
 template "/etc/haproxy/haproxy.cfg" do
   source "haproxy.cfg.erb"
-  variables ({
-    :web_servers = node['nateApp']['webservers']
+  variables({
+    :web_servers => node['nateApp']['webservers']
   })
   notifies :restart, "service[haproxy]"
 end
