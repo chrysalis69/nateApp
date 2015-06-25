c42e37104fe63e214ceb01ee6e55dc5142d4fe18
Stefan Coetzee <stefan.coetzee@uct.ac.za>
2015-06-25 11:02:53 +0200

    Changed to template

--- recipes/proxy_server.rb
+++ recipes/proxy_server.rb
@@ -1,18 +1,9 @@
-web_servers_ips = node['nateApp']['webservers']
-
-web_servers = []
-web_servers_ips.each do |web_server|
-  web_servers.push("#{web_server} #{web_server}:8080 check")
-end
-
 include_recipe "haproxy::install_package"
 
-haproxy_lb 'nateAppWeb' do
-  bind '0.0.0.0:80'
-  mode 'http'
-  servers web_servers
-  params({
-    'maxconn' => 20000,
-    'balance' => 'leastconn'
+template "/etc/haproxy/haproxy.cfg" do
+  source "haproxy.cfg.erb"
+  variables ({
+    :web_servers = node['nateApp']['webservers']
   })
+  notifies :restart, "service[haproxy]"
 end
