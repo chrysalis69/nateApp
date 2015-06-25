d764a97ff113cd604f6084abe5712865ac2a1e4d
Stefan Coetzee <stefan.coetzee@uct.ac.za>
2015-06-25 10:47:59 +0200

    Not sure

--- recipes/proxy_server.rb
+++ recipes/proxy_server.rb
@@ -1,13 +1,16 @@
-web_servers = node['nateApp']['webservers']
+web_servers_ips = node['nateApp']['webservers']
+
+web_servers = []
+web_servers_ips.each do |web_server|
+  web_servers.push("#{web} #{web}:8080 check")
+end
 
 include_recipe "haproxy::install_package"
 
 haproxy_lb 'nateAppWeb' do
   bind '0.0.0.0:80'
   mode 'http'
-  servers web_servers.map do |web|
-    "#{web} #{web}:8080 check"
-  end
+  servers web_servers
   params({
     'maxconn' => 20000,
     'balance' => 'leastconn'
