a6cbaff7495c3c150dae5d9900b1a2116efb688e
Stefan Coetzee <stefan.coetzee@uct.ac.za>
2015-06-25 11:34:49 +0200

    Typo

--- recipes/proxy_server.rb
+++ recipes/proxy_server.rb
@@ -5,7 +5,7 @@ package "haproxy" do
 end
 ruby_block "Enable HaProxy" do
   block do
-    fe = Chef::Util::FileEdit.new("/etc/defaults/haproxy")
+    fe = Chef::Util::FileEdit.new("/etc/default/haproxy")
     fe.search_file_replace(/ENABLED=0/,"ENABLED=1")
     fe.write_file
   end
